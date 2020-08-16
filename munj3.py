a9565939a128b34585459a8bc654a957982b033c
alex <woongrae21@hanmail.net>
2020-08-16 13:04:17 +0900

    new

---
+++ munj3.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+k = (a ** b)
+j = (c ** d)
+print(k + j)
+
+
